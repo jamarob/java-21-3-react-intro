1683d46cc349ad6c72e301ba4550282417a05f94
Jan Robert <janmartinrobert@gmail.com>
2021-08-04 16:19:21 +0200

    Stub out CharacterCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Header from './components/Header'
+import characterResponse from './characters-response.json'
 
 function App() {
+    const characters = characterResponse.results
+
     return <div>
         <Header title="Character Gallery"/>
+        <CharacterCard character={characters[0]} />
+        <CharacterCard character={characters[1]} />
     </div>
 }
 
