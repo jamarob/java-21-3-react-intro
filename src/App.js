dd976fd73279b2eecafbecace620b1be1a57cdc3
Jan Robert <janmartinrobert@gmail.com>
2021-08-04 16:13:26 +0200

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
-    return <h1>hello world</h1>
+    return <div>
+        <Header title="Character Gallery"/>
+    </div>
 }
 
 export default App;
