c1866a5e0a517f5c02e8fe84585e3ba10fde3617
Jan Robert <janmartinrobert@gmail.com>
2021-08-05 13:02:15 +0200

    Use state for characters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css'
 import Header from './components/Header'
 import CharacterGallery from './components/CharacterGallery'
 import characterResponse from './characters-response.json'
+import { useState } from 'react'
 
 function App() {
-  const characters = characterResponse.results
+  const [characters, setCharacters] = useState([])
+
+  const handleClick = () => {
+    setCharacters(characterResponse.results)
+  }
 
   return (
     <div>
       <Header title="Character Gallery" />
+      <button onClick={handleClick}>Load characters</button>
       <CharacterGallery characters={characters} />
     </div>
   )
